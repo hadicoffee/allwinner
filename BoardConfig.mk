a79339192baff8aac2185506084ae97e230c8c34
hadicoffee <hadikusumaandroid@gmail.com>
2013-03-14 00:55:34 +0800

    firs commit

---
+++ BoardConfig.mk
@@ -0,0 +1,31 @@
+USE_CAMERA_STUB := false
+
+# inherit from the proprietary version
+-include vendor/allwinner/a10/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := exDroid
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := a10
+
+BOARD_KERNEL_CMDLINE := no_console_suspend
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 00000800
+
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := device/allwinnerr/a10/recovery_keys.c
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/allwinner/a10/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
